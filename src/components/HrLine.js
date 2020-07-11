105a20b086d94d99688f09c8e6ed5a4c53f4ce53
iamraffe <raffe90@gmail.com>
2020-07-11 18:00:05 +0200

    Fix on missing import (redux)

--- src/components/HrLine.js
+++ src/components/HrLine.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, TouchableOpacity } from "react-native";
 import PropTypes from "prop-types";
 import { AppContext } from "./ContextProvider";
 
