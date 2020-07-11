60d6c13f9b91bc0c058dd75ff36829ba769f9d6a
iamraffe <raffe90@gmail.com>
2020-07-11 21:45:48 +0200

    WIP: fix on undef ret

--- src/services/todayData.js
+++ src/services/todayData.js
@@ -1,20 +1,24 @@
-import procRows from './procRows';
+import procRows from "./procRows";
 
 const todayData = (dataArray, date) => {
-  if (!dataArray) { return [] }
+  if (!dataArray) {
+    return [];
+  }
 
-  const today = dataArray.filter(appt => {
-    ret = sameDay(appt.start, date);
-    ret1 = sameDay(appt.end, date);
+  const today = dataArray.filter((appt) => {
+    let ret = sameDay(appt.start, date);
+    let ret1 = sameDay(appt.end, date);
     return ret || ret1;
   });
   return procRows(today);
-}
+};
 
 const sameDay = (d1, d2) => {
-  return d1.getDate() === d2.getDate() &&
-  d1.getMonth() === d2.getMonth() && 
-  d1.getFullYear() === d2.getFullYear();
-}
+  return (
+    d1.getDate() === d2.getDate() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getFullYear() === d2.getFullYear()
+  );
+};
 
 export default todayData;
