a653beb2727691734be6d060cfd787e8b8a48d3b
iamraffe <raffe90@gmail.com>
2020-07-12 17:36:08 +0200

    Remove console.log

--- src/services/procRows.js
+++ src/services/procRows.js
@@ -1,30 +1,33 @@
-import {hrsToStart} from './hrsToPx';
+import { hrsToStart } from "./hrsToPx";
 
-const procRows = (inputData) => { // Predicated on appts sorted by start
+const procRows = (inputData) => {
+  // Predicated on appts sorted by start
   let outputData = [];
 
-  inputData.forEach(appt => {
+  inputData.forEach((appt) => {
     let index = outputData.findIndex((el) => overlaps(appt, el));
 
     if (index >= 0) {
-      outputData[index].rowAppts.push(appt);;
-      outputData[index].end = addToEnd(outputData[index].end, appt.end)
+      outputData[index].rowAppts.push(appt);
+      outputData[index].end = addToEnd(outputData[index].end, appt.end);
     } else {
       outputData.push({
         hrsBefore: hrsToStart(appt.start),
         start: appt.start,
         end: appt.end,
-        rowAppts: [appt]
-      })
+        rowAppts: [appt],
+      });
     }
-    console.log('output data', outputData)
-  })
+    // console.log('output data', outputData)
+  });
 
   return outputData;
-}
+};
 
-const overlaps = (a, b) => (a.start <= b.start && b.start <= a.end) || (a.start <= b.end && b.end <= a.end);
+const overlaps = (a, b) =>
+  (a.start <= b.start && b.start <= a.end) ||
+  (a.start <= b.end && b.end <= a.end);
 
-const addToEnd = (sumEnd, addedEnd) => sumEnd > addedEnd ? sumEnd : addedEnd
+const addToEnd = (sumEnd, addedEnd) => (sumEnd > addedEnd ? sumEnd : addedEnd);
 
 export default procRows;
